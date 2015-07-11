c0ef51f46f7dcce2dbbb5ead9dd2419fe873b02f
barld <bakeds@gmail.com>
2015-07-11 17:21:12 +0200

    add problem 2

---
+++ EulerFSharp/problem2.fs
@@ -0,0 +1,10 @@
+﻿module problem2
+    let answer =
+        let fibSeq = 
+            let even x = x%2=0 
+            Seq.unfold (fun (a,b) -> Some( a+b, (b, a+b))) (1,2)
+            |> Seq.takeWhile (fun x -> x < 4000000)
+            |> Seq.filter even
+        fibSeq |> Seq.sum
+        
+        

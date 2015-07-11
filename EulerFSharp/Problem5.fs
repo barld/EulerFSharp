f8b08dc819d2cb89d0ca8efc2bdef5916f7494fa
barld <bakeds@gmail.com>
2015-07-11 21:51:33 +0200

    add problem 5

---
+++ EulerFSharp/Problem5.fs
@@ -0,0 +1,11 @@
+﻿module Problem5
+    let rec filter n s =
+        match n with
+        | x when x < 2 -> s
+        | _ -> s |> Seq.filter (fun x -> x % n = 0) |> filter (n-1)
+        
+    let answer =
+        Seq.unfold (fun x -> Some(x, x+20)) 20
+        |> filter 19
+        |> Seq.head
+

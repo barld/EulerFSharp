cd0ec6ea963f4eb79e3e661a64094b2539f026b3
barld <bakeds@gmail.com>
2016-02-16 23:52:29 +0100

    add problem 6

---
+++ EulerFSharp/problem6.fs
@@ -0,0 +1,13 @@
+﻿module problem6
+    
+    let oneToHunderd = {1..100}
+    let square x = x*x
+
+    let sumOfSquares = oneToHunderd |> Seq.map square |> Seq.sum
+    let SquareOfSum = oneToHunderd |> Seq.sum |> square
+
+    let answer = SquareOfSum - sumOfSquares
+
+
+
+
